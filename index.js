8619c32e6971910411464186a1926ce71b02456a
Liam Allan <WorksOfLiam@users.noreply.github.com>
2018-10-13 04:28:00 +0100

    IBM Cloud stuff

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const yum = require('./yum');
 const express = require('express');
 const app = express();
-const port = 3000;
+
+var cfenv = require('cfenv');
+var appEnv = cfenv.getAppEnv();
 
 var remoteFTP = "public.dhe.ibm.com";
 var remoteFile = "/software/ibmi/products/pase/rpms/repo/repodata/d75d4b240e89c9c3cf13f1f7d5d35839a1a20335f8799f4cab5fd0e443e6200c-primary.xml.gz";
 
 var jsonData = {};
 
-app.listen(port, function() {
+app.listen(appEnv.port, function() {
   yum.downloadGZ(remoteFTP, remoteFile, function(err, xmlPath) {
     yum.xmlToJSON(xmlPath, function(err, json) {
       jsonData = yum.parseRepoJson(json);
-    })
+    });
   });
+
+  console.log("server starting on " + appEnv.url);
 });
 
 app.get('/repo', function(req, res) {
